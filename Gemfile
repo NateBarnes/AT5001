e9d0cf700d6dc42f26d7b427c80ec0e21731a997
Nathaniel Barnes <kerianambrai@gmail.com>
2011-08-22 23:23:15 -0500

    Initial Commit

---
+++ Gemfile
@@ -0,0 +1,4 @@
+source "http://rubygems.org"
+
+gem "adhearsion", ">= 1.2"
+gem "rails"
---
+++ gui/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Gui::Application.config.secret_token = 'cf582dea2d6498204d9c5d9e8454030e0a9945500ee9d1c158f8c028f427a689f49f66264901f89335bba10f03f313bddb95a3050ed9731627f4269e82674bae'
