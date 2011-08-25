f9178f2dc19d1da962c87abbc36580b99dec59e3
Nathaniel Barnes <kerianambrai@gmail.com>
2011-08-24 23:06:37 -0500

    Added more wildcards to the queueing class

--- lib/call_job.rb
+++ lib/call_job.rb
@@ -6,8 +6,18 @@ class CallJob
   @token = YAML.load_file("config/at5001_config.yml")["provider"]["token"]
   
   def self.perform number
-    res = Net::HTTP.post_form(URI.parse('http://api.tropo.com/1.0/sessions'), "token" => @token,
-                                      "destination" => number)
+    tries = 0
+    begin
+      res = Net::HTTP.post_form(URI.parse('http://api.tropo.com/1.0/sessions'), "token" => @token,
+                                          "destination" => number)
+    rescue SocketError => se
+      if tries < 3
+        tries += 1
+        retry
+      else
+        puts se
+      end
+    end
   end
   
 end
--- lib/queue_calls.rb
+++ lib/queue_calls.rb
@@ -4,6 +4,14 @@ class QueueCalls
   def self.parse_string num
     if num.include? "*"
       (0..9).each { |i| parse_string num.sub("*", i.to_s) }
+    elsif num.include? "["
+      set = num.slice num.index("[")+1, num.index("]")-num.index("[")-1
+      if set.include? "-"
+        min, max = set.split("-")
+        (Integer(min)..Integer(max)).each { |i| parse_string num.sub( "["+set+"]", i.to_s )}
+      elsif set.include? ","
+        set.split(",").each { |s| parse_string num.sub( "["+set+"]", s )}
+      end
     else
       puts num
     end
